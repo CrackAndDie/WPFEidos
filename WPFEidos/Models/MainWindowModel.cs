d886c6cb4bcccab34baf88d5ffd92b2e1ca6a648
Airat <abdrakovairat@gmail.com>
2023-06-05 16:05:10 +0300

    Base logic done

---
+++ WPFEidos/Models/MainWindowModel.cs
@@ -0,0 +1,68 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace WPFEidos.Models
+{
+    internal class MainWindowModel : BindableBase
+    {
+        private Page windowContent;
+        public Page WindowContent 
+        {
+            get { return windowContent; } 
+            set { SetProperty(ref windowContent, value); } 
+        }
+
+        #region Commands
+        public ICommand EmployeesCommand { get; set; }
+        public ICommand DepartmentsCommand { get; set; }
+
+        public ICommand OpenFileCommand { get; set; }
+        public ICommand SaveFileCommand { get; set; }
+        public ICommand ExitCommand { get; set; }
+        #endregion
+
+        public MainWindowModel()
+        {
+            EmployeesCommand = new DelegateCommand(OnEmployeesCommand);
+            DepartmentsCommand = new DelegateCommand(OnDepartmentsCommand);
+
+            OpenFileCommand = new DelegateCommand(OnOpenFileCommand);
+            SaveFileCommand = new DelegateCommand(OnSaveFileCommand);
+            ExitCommand = new DelegateCommand<object>(OnExitCommand);
+        }
+
+        private void OnEmployeesCommand()
+        {
+
+        }
+
+        private void OnDepartmentsCommand()
+        {
+
+        }
+
+        private void OnOpenFileCommand()
+        {
+            
+        }
+
+        private void OnSaveFileCommand()
+        {
+            
+        }
+
+        private void OnExitCommand(object parameter)
+        {
+            (parameter as Window).Close();
+        }
+    }
+}
---
+++ WPFEidos/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using WPFEidos.Models;
+
+namespace WPFEidos.ViewModels
+{
+    internal class MainWindowViewModel : BindableBase
+    {
+        private MainWindowModel model = new MainWindowModel();
+
+        public Page WindowContent => model.WindowContent;
+
+        #region Commands
+        public ICommand EmployeesCommand => model.EmployeesCommand;
+        public ICommand DepartmentsCommand => model.DepartmentsCommand;
+
+        public ICommand OpenFileCommand => model.OpenFileCommand;
+        public ICommand SaveFileCommand => model.SaveFileCommand;
+        public ICommand ExitCommand => model.ExitCommand;
+        #endregion
+
+        public MainWindowViewModel()
+        {
+            model.PropertyChanged += (s, e) => { RaisePropertyChanged(e.PropertyName); };
+        }
+    }
+}
